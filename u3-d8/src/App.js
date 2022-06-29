87fb39aff0c040e4f7f8750827f44a80730b96f8
marios <mgeorgak859@gmail.com>
2022-06-29 15:44:59 +0300

    changes

--- u3-d8/src/App.js
+++ u3-d8/src/App.js
@@ -1,24 +1,21 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
-import NavBar from './components/NavBar';
-import GalleryOne from './components/GalleryOne';
-import GalleryTwo from './components/GalleryTwo'
-import GalleryThree from './components/GalleryThree'
-import TvShows from './components/TvShows';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+import GalleryOne from "./components/GalleryOne";
+import GalleryTwo from "./components/GalleryTwo";
+import GalleryThree from "./components/GalleryThree";
+import TvShows from "./components/TvShows";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <NavBar />
-    <div>
-      
-   <TvShows />
-      
-
-    </div>
-    </BrowserRouter>
+      <BrowserRouter>
+        <NavBar />
+        <div>
+          <TvShows />
+        </div>
+      </BrowserRouter>
     </>
   );
 }
